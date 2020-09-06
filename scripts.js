5de1e79dd29ea32b4059f2f196df8bb1744cf937
Carlos <jjbbpe@gmail.com>
2020-09-06 20:17:44 +0200

    aa

--- scripts.js
+++ scripts.js
@@ -1,43 +1,41 @@
-
-var botones = document.querySelectorAll("nav a")
-
-botones.addEventListener('click', {
-     for( var i=0; i<botones.length; i++){
-      console.log(botones[i].getAttribute('id'))
-      }
-})
+function clear() {
+     var main = document.querySelector('#main section');
+     main.innerHTML = ` `
+}
 
 
-// function descargarBikini() {
-//     const apo = `https://pixabay.com/api/?key=18166422-c39881a9559fd66a7d6c00c5c&q=bikini&per_page=3&image_type=photo&pretty=true`
-//
-//     // llamado Fetch
-//     fetch(apo)
-//       .then(respuesta => respuesta.json() )
-//       .then(datos => {
-//            imprimirHTML(datos.hits );
-//            console.log(datos.hits)
-//       });
-// }
-//
-// function imprimirHTML(datos){
-//      datos.forEach(usuario => {
-//
-//           const li = document.createElement("li");
-//
-//           const {tags,userImageURL } = usuario;
-//
-//           li.innerHTML = `
-//           ID: ${tags} <br>
-//           <img src="${userImageURL}" style=width:100px>
-//           `;
-//
-//           document.querySelector("#box_fetch").appendChild(li);
-//      });
-// }
-//
-// const boton_fetch = document.querySelector("#boton_fetch");
-// boton_fetch.addEventListener('click', descargarBikini);
+
+function descargarBikini() {
+    const apo = `https://pixabay.com/api/?key=18166422-c39881a9559fd66a7d6c00c5c&q=bikini&per_page=3&image_type=photo&pretty=true`
+
+    // llamado Fetch
+    fetch(apo)
+      .then(respuesta => respuesta.json() )
+      .then(datos => {
+           imprimirHTML(datos.hits );
+           console.log(datos.hits)
+      });
+
+
+
+}
+
+function imprimirHTML(datos){
+
+     datos.forEach(usuario => {
+          const li = document.createElement("li");
+
+          const {tags,userImageURL } = usuario;
+
+          li.innerHTML = `
+          ID: ${tags} <br>
+          <img src="${userImageURL}" style=width:100px>
+          `;
+
+          document.querySelector("#box_fetch").appendChild(li);
+     });
+}
+
 
 
 // ............................ Object Constructor
@@ -46,7 +44,8 @@ class Cancion {
      constructor(color){
           this.color = color;
           this.añadir = function(nombre){
-               const box_body = document.querySelector("#box_constructor");
+               const box_constructor = document.querySelector("#box_constructor");
+
                const contenido = `
                                <div style=   'float:left;
                                               width: calc(33.3% - 2rem);
@@ -58,7 +57,7 @@ class Cancion {
                                     <h4 style="color:white"> ${nombre} <h4>
                                </div>
                                `
-                          box_body.innerHTML += contenido
+                          box_constructor.innerHTML += contenido
           }
      }
 }
@@ -67,9 +66,6 @@ const facil = new Cancion("green")
 // const medio = new Cancion()
 // const dificil = new Cancion()
 
-
-const constructor = document.querySelector("#boton_constructor");
-constructor.addEventListener('click', añadir);
-function añadir(){
+function añadir_cancion(){
      facil.añadir("cancion fácil");
 }
